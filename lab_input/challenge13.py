5380e1b1438cb2c4b60530f8f2122edd2ca8883e
Rackheath22 <doesntmatter@tlgcohort.com>
2023-02-15 20:45:59 +0000

    push

---
+++ lab_input/challenge13.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   Challenge: Shbang, Input, Print, Concatenate, Variables"""
+
+def main():
+
+    # collect string input from user
+    user_Name = input("Hello! What is your name?")
+
+    # collect string input from user
+    Day_of_Week = input("What day of the week is it today?")
+
+    ## the line below creates a single string that is passed to print()
+    # print("Hello, ": " + user_Name)
+    # print("Happy ": " + Day_of_Week)
+
+    
+
+main()
